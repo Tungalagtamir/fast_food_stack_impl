ca07ef27f210e9af2e20ba543ec68a3fe1b713ae
Tamir <79493337+Tungalagtamir@users.noreply.github.com>
2025-10-20 09:00:00 +0100

    Initialize project structure

---
+++ src/ca1_2025168_fast_food/CA1_2025168_Fast_Food.java
@@ -0,0 +1,7 @@
+package ca1_2025168_fast_food;
+
+public class CA1_2025168_Fast_Food {
+    public static void main(String[] args) {
+        System.out.println("Fast Food Storage System - CA1_2025168");
+    }
+}
